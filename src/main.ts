8b13ff240dd86eed7956b05a7c11654bb8dc8537
babe25 <babehndr5@gmail.com>
2024-07-23 02:20:24 +0700

    modified: Port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
 
-  await app.listen(3000);
+  await app.listen(8080);
 }
 bootstrap();
