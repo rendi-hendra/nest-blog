1137ff5a7b94f3ee809822cb666c41a18785bacd
babe25 <babehndr5@gmail.com>
2024-07-27 20:35:23 +0700

    fix: Update .env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function bootstrap(config: ConfigService) {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
 
-  await app.listen(8080);
+  await app.listen(config.get('PORT'));
 }
-bootstrap();
+const configService = new ConfigService();
+bootstrap(configService);
